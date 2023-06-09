4959f605441107e64c82519ae0361a2d97643349
Mustafa Saifee <saifeemustafaq@gmail.com>
2023-06-09 16:50:26 +0530

    Update AnalyzePhoto.vue

--- src/components/AnalyzePhoto.vue
+++ src/components/AnalyzePhoto.vue
@@ -144,6 +144,7 @@ export default {
                         credentials: {
                             accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
                             secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
+                            sessionToken: import.meta.env.VITE_AWS_SESSION_TOKEN,
                         },
                     });
                     console.log(client)
