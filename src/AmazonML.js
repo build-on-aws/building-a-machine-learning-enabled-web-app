6f783fc6ea7e85c8afcf67253299e12fb1928dbe
Stephen Howell <121949113+srhowell@users.noreply.github.com>
2023-11-21 16:11:56 +0000

    Update AmazonML.js fix logic error

--- src/AmazonML.js
+++ src/AmazonML.js
@@ -2,55 +2,58 @@ import { Buffer } from "buffer";
 import { RekognitionClient, DetectLabelsCommand } from "@aws-sdk/client-rekognition";
 import { DetectDocumentTextCommand, TextractClient } from "@aws-sdk/client-textract";
 
-let rekognitionClient = null;
-let textractClient = null;
-
-// Check README.md for instructions on configuring the required local file
 const creds = {
   region: import.meta.env.VITE_AWS_REGION,
   credentials: {
     accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
     secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
+    sessionToken: import.meta.env.VITE_AWS_SESSION_TOKEN, // leave out if not in hosted workshop
   },
 };
 
+let rekognitionClient = null;
+let textractClient = null;
+
 export async function analyzeImageML(type, imageData) {
-  // Encode the image bytes as uint8
-  const uimage_bytes = base64ToUint8Array(
-    imageData.split("data:application/octet-stream;base64,")[1]
-  );
-  let response = null;
+  const uimage_bytes = base64ToUint8Array(imageData.split("data:application/octet-stream;base64,")[1]);
+  const params = {
+    Image: { Bytes: uimage_bytes },
+    MaxLabels: 10,
+    MinConfidence: 80,
+  };
+  
+  var returnData = null;
   try {
     if (type == "labels") {
-      if (!rekognitionClient) rekognitionClient = new RekognitionClient(creds);
-      const params = {
-        Image: { Bytes: uimage_bytes },
-        MaxLabels: 10,
-        MinConfidence: 80,
-      };
+      // If the client has not been initalized yet, create it
+      if (!rekognitionClient)  
+        rekognitionClient = new RekognitionClient(creds); // pass in the creds as parameter
       const query = new DetectLabelsCommand(params);
-      response = await rekognitionClient.send(query);
-    } else if (type == "text") {
-      if (!textractClient) textractClient = new TextractClient(creds);
+      let response = await rekognitionClient.send(query);        
+      returnData = {
+        type: "success",
+        text: response,
+      };    
+  } else if (type == "text") {
+      if (!textractClient) 
+        textractClient = new TextractClient(creds);
       const params = {
         Document: { Bytes: uimage_bytes },
-      };
+      };      
       const query = new DetectDocumentTextCommand(params);
-      response = await textractClient.send(query);
+      let response = await textractClient.send(query);
+      returnData = {
+        type: "success",
+        text: response,
+      };  
     }
   } catch (error) {
-    const errorData = {
+    returnData = {
       type: "error" /* success info warning error */,
       text: error.message,
-    };
-    return JSON.stringify(errorData);
-  } finally {
-    const returnData = {
-      type: "success",
-      text: response,
-    };
-    return JSON.stringify(returnData);
+    };    
   }
+  return JSON.stringify(returnData);  
 }
 
 // imageData is string with data:application/octet-stream;base64,...
